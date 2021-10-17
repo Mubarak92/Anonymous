4d35e0336b5eca916ee05f12258a5b57c1142740
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-18 01:07:22 +0300

    passing trailing lambda

--- src/main/kotlin/TheCity.kt
+++ src/main/kotlin/TheCity.kt
@@ -4,13 +4,12 @@ import kotlin.random.nextInt
 
 fun main() {
 
-    var  NationalDay1={month:String,num:Int ->
+    random("september"){month,num ->
         val nationalDay = 23
         println("city number $num")
         println( "National day of Saudi arabia is in $nationalDay of $month")
 
     }
-    random("september",NationalDay1)
 
 }
 fun random(month: String, NationalDay1: (String, Int) -> Unit) {
