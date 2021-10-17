05c60485e0c2b6bf185805d389a760ad2bd394e7
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-18 01:17:02 +0300

    .

--- src/main/kotlin/TheCity.kt
+++ src/main/kotlin/TheCity.kt
@@ -12,7 +12,15 @@ fun main() {
     }
 
 }
-fun random(month: String, NationalDay1: (String, Int) -> Unit) {
+inline fun random(
+    month: String,
+    cost:(Int) ->
+    NationalDay1: (String, Int) -> ) {
     val num = Random.nextInt(1..3) // Randomly selects 1, 2, or 3
     println(NationalDay1(month, num))
+}
+
+fun printConstructionCost(num: Int) {
+    val cost = 500
+    println("construction cost: ${cost * num}")
 }
