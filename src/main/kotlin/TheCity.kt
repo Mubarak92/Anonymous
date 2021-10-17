e48c7bef09ec034c3efb46c8d2da9c7a86647592
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-18 01:05:54 +0300

    generate random number

--- src/main/kotlin/TheCity.kt
+++ src/main/kotlin/TheCity.kt
@@ -1,10 +1,19 @@
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 fun main() {
 
-    val NationalDayFunction :(String) -> String = {
+    var  NationalDay1={month:String,num:Int ->
         val nationalDay = 23
-
-        "National day of Saudi arabia is in $nationalDay of $it"
+        println("city number $num")
+        println( "National day of Saudi arabia is in $nationalDay of $month")
 
     }
-    println(NationalDayFunction("September"))
+    random("september",NationalDay1)
+
+}
+fun random(month: String, NationalDay1: (String, Int) -> Unit) {
+    val num = Random.nextInt(1..3) // Randomly selects 1, 2, or 3
+    println(NationalDay1(month, num))
 }
