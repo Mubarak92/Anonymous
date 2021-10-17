1396c3c5f35641b092f954fe5c7887c3113b83cc
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-18 00:00:50 +0300

    parameter to the anonymous

--- src/main/kotlin/TheCity.kt
+++ src/main/kotlin/TheCity.kt
@@ -1,10 +1,10 @@
 fun main() {
 
-    val NationalDayFunction :(String) -> String = {  ruler->
+    val NationalDayFunction :(String) -> String = {  month->
         val nationalDay = 23
-        val month = "September "
+
         "National day of Saudi arabia is in $nationalDay of $month"
 
     }
-    println(NationalDayFunction("King Salman"))
+    println(NationalDayFunction("September"))
 }
