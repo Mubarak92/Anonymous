2246834ce2243fba3fff866e22dea137038a0c90
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-17 17:56:38 +0300

    Assigning the anonymous function to a variable

--- src/main/kotlin/TheCity.kt
+++ src/main/kotlin/TheCity.kt
@@ -1,10 +1,10 @@
 fun main() {
-    println(
-        {
-val nationalDay = 23
-            val month = "September "
-            "National day of Saudi arabia is in $nationalDay of $month"
 
-        }()
-    )
+    val NationalDayFunction :() -> String = {
+        val nationalDay = 23
+        val month = "September "
+        "National day of Saudi arabia is in $nationalDay of $month"
+
+    }
+    println(NationalDayFunction)
 }
