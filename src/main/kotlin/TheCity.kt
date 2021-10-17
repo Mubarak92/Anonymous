901926137ebb583afd42058734688c1319d39a39
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-17 17:51:55 +0300

    Defining an anonymous function

---
+++ src/main/kotlin/TheCity.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println(
+        {
+val nationalDay = 23
+            val month = "September "
+            "National day of Saudi arabia is in $nationalDay of $month"
+
+        }()
+    )
+}
